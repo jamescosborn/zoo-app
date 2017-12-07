beb31fc8ea806b31a610f69cb9a129bb8786c1ed
Guest User <Guest@Epicodus-4A.local>
2017-12-07 14:04:50 -0800

    add component nesting and pipe.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,74 +1,43 @@
 import { Component } from '@angular/core';
+import { Task } from './task.model';
 
 @Component({
   selector: 'app-root',
   template: `
-  <div class="container">
-    <h1>To Do List for {{month}}/{{day}}/{{year}}</h1>
-    <h3>For Epicodus {{currentFocus}}</h3>
-    <ul>
-      <li [class]="priorityColor(currentTask)" (click)="isDone(currentTask)" *ngFor="let currentTask of tasks">{{currentTask.description}} <button (click)="editTask(currentTask)">Edit!</button></li>
-    </ul>
-    <hr>
-      <div *ngIf="selectedTask">
-        <h3>{{selectedTask.description}}</h3>
-        <p>Task Complete? {{selectedTask.done}}</p>
-        <h3>Edit Task</h3>
-        <label>Enter Task Description:</label>
-        <input [(ngModel)]="selectedTask.description">
-        <label>Enter Task Priority (1-3):</label>
-        <br>
-        <input type="radio" [(ngModel)]="selectedTask.priority" [value]="1">1 (Low Priority)<br>
-        <input type="radio" [(ngModel)]="selectedTask.priority" [value]="2">2 (Medium Priority)<br>
-        <input type="radio" [(ngModel)]="selectedTask.priority" [value]="3">3 (High Priority)
-        <button (click)="finishedEditing()">Done</button>
-     </div>
-  </div>
+    <div class="container">
+      <h1>To Do List for {{month}}/{{day}}/{{year}}</h1>
+      <h3>{{currentFocus}}</h3>
+      <task-list [childTaskList]="masterTaskList" (clickSender)="editTask($event)"></task-list>
+      <hr>
+      <edit-task [childSelectedTask]="selectedTask" (doneButtonClickedSender)="finishedEditing()"></edit-task>
+      <new-task (newTaskSender)="addTask($event)"></new-task>
+    </div>
   `
 })
 
 export class AppComponent {
-currentFocus: string = "Angular Homework"
-currentTime = new Date();
-month: number = this.currentTime.getMonth() + 1;
-day: number = this.currentTime.getDate();
-year: number = this.currentTime.getFullYear();
-tasks: Task[] = [
-  new Task("Finish weekend Angular homework for Epicodus course", 3),
-  new Task('Begin brainstorming possible JavaScript group projects', 2),
-  new Task('Add README file to last few Angular repos on GitHub', 2)
-];
+  currentFocus: string = "Angular Homework"
+  currentTime = new Date();
+  month: number = this.currentTime.getMonth() + 1;
+  day: number = this.currentTime.getDate();
+  year: number = this.currentTime.getFullYear();
   selectedTask = null;
 
-priorityColor(currentTask) {
-  if (currentTask.priority === 3) {
-    return "bg-danger";
-  } else if (currentTask.priority === 2) {
-    return "bg-warning";
-  } else {
-    return "bg-info";
+  masterTaskList: Task[] = [
+    new Task("Finish brainwashing Kenny the cult member", 3),
+    new Task('Begin brainwashing the newest initiate', 2),
+    new Task('Research which soap is best for brainwashing', 2)
+  ];
+
+  editTask(clickedTask) {
+    this.selectedTask = clickedTask;
   }
-}
 
-editTask(clickedTask) {
-  this.selectedTask = clickedTask;
-}
-
-isDone(clickedTask: Task) {
-  if(clickedTask.done === true) {
-    alert("This task is done!");
-  } else {
-    alert("This task is not done. Better get to work!")
+  finishedEditing() {
+    this.selectedTask = null;
   }
-}
 
-finishedEditing() {
-  this.selectedTask = null;
-}
-
-}
-
-export class Task {
-  public done: boolean = false;
-  constructor(public description: string, public priority: number) { }
+  addTask(newTaskFromChild: Task) {
+    this.masterTaskList.push(newTaskFromChild);
+  }
 }
